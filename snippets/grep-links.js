80770e739d679884477da5983c6c76b0ea2752aa
Gopinath Kesavan <gopinath.k@myntra.com>
2017-12-11 18:06:12 +0530

    space formats

--- snippets/grep-links.js
+++ snippets/grep-links.js
@@ -1,13 +1,13 @@
 (function() {
-   console.clear();
-   var nodes = document.getElementsByTagName('a');
-//    console.log('nodes', nodes);
-   var map = Array.prototype.map;
-   var linksAll = map.call(nodes, function(item) { return item.href });
-//    console.log('linksAll', linksAll);
-   var links = linksAll.filter(function(item) { 
-//         console.log(item);
-        return /http:\/\/www.tamilmp3online.com\/download\.php/.test(item)
-   });
-   console.log(links.join("\n")); 
+	console.clear();
+	var nodes = document.getElementsByTagName('a');
+	// console.log('nodes', nodes);
+	var map = Array.prototype.map;
+	var linksAll = map.call(nodes, function(item) { return item.href });
+	// console.log('linksAll', linksAll);
+	var links = linksAll.filter(function(item) {
+		// console.log(item);
+		return /\.zip/.test(item)
+	});
+	console.log(links.join("\n"));
 }())
--- snippets/wipe.js
+++ snippets/wipe.js
@@ -1,4 +1,6 @@
-var s = prompt("enter the css selector"),
+Array.from(document.querySelectorAll('.select-content-language')).forEach(node => node.parentNode.removeChild(node))
+//var s = prompt("enter the css selector"),
+var s = '.section-question',
 		d = document,
 		n = d.querySelector(s),
     k = [].slice;
